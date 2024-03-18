1477051c68b298fa426f52a231cafcac7aba8d92
Michael Mo <michaelmoshui@gmail.com>
2024-03-18 02:09:53 -0400

    Update actual location format in degrees and set transmitter bit length to 128

--- communication_functions/signal_transmission.py
+++ communication_functions/signal_transmission.py
@@ -2,6 +2,7 @@ from external_packages.rpi_rf.rpi_rf import RFDevice
 
 # Input Parameters:
 # location ~ a string of digits representing the coordinate of the device
+#          ~ in the format NXXXYYYYYYYYYDMAAABBBBBBBBBE (More details in get location function)
 # status ~ a single char - "1" or "0" - which denotes if motion signal is detected or not detected, respectively
 
 # Output Parameters: NONE
@@ -12,12 +13,12 @@ def send_signal(location, status):
     rf_transmitter.enable_tx()
 
     # concatenate location and status and convert it into an encoded decimal value
-    # FORMAT: (int) XXXXXXXXS, where X's encode for location info and S encodes for status info
+    # FORMAT: (int) NX...BES, where N -> E encode for location info and S encodes for status info
     transmission_info = int(location + status)
 
     # Use the tx_code function in radio frequency transmitter
     # to broadcast the encoded location and status information.
-    rf_transmitter.tx_code(transmission_info)
+    rf_transmitter.tx_code(code=transmission_info, tx_length=128)
 
     # Disable the transmitter device
     rf_transmitter.cleanup()
@@ -27,7 +28,7 @@ def send_signal(location, status):
 # rf_receiver ~ the receiver device initiated in the main code.py file
     
 # Output Parameters
-# recevier code: a string "XXXXXXS",
-# where X's denote location of signal source and S denotes motion sensing status of signal source
+# recevier code: a string "N...ES" that denotes the location of signal source and S denotes motion sensing status 
+# (More details about N -> E location signal in get location section)
 def receive_signal(rf_receiver):
     return str(rf_receiver.rx_code)
