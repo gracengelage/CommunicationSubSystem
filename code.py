216a23421f1fa42024f7f64eb7632b64286eedd9
GW314 <1wugab@hdsb.ca>
2024-03-19 18:28:37 -0400

    rpi_rf

--- code.py
+++ code.py
@@ -1,7 +1,12 @@
 import time
-import board
-import digitalio
+# import board
+# import digitalio
 from communication_functions.get_location import get_location
 
+from external_packages.rpi_rf import RFDevice
+
+rfdevice = RFDevice(17)
+rfdevice.enable_rx()
+
 while True:
-    pass
+    receive_signal(rfdevice) 
