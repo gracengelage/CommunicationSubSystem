ab26a2eb411d1c00c4f65ded09e51196645b1c21
Michael Mo <michaelmoshui@gmail.com>
2024-03-20 15:10:45 -0400

    GPS fricking works!!!!!

--- code.py
+++ code.py
@@ -1,9 +1,7 @@
 import time
-import board
-import digitalio
 from communication_functions.get_location import get_location
 
-from external_packages.rpi_rf.rpi_rf import RFDevice
-
-rfdevice = RFDevice(17)
-rfdevice.enable_rx()
+while True:
+    location_received = ""
+    print(get_location(location_received))
+    time.sleep(1)
