f477a3cf27705ff30432eefabeb7ec83462c37aa
Grace Engelage <grace.engelage@gmail.com>
2024-03-20 18:57:08 -0400

    LED lights added - pins can be changed

--- communication_functions/update_state.py
+++ communication_functions/update_state.py
@@ -1,8 +1,18 @@
 import time
+import board
+import digitalio
 from external_packages.custom_rf.receiver import Receiver
 from communication_functions.get_location import get_location
 from communication_functions.signal_transmission import receive_signal
 from communication_functions.calculate_distance import calculate_distance
+
+# This LED represents the big light
+LIGHT = digitalio.DigitalInOut(board.GP23)
+LIGHT.direction = digitalio.Direction.OUTPUT
+
+# This LED represents the indicator that a signal has been recieved
+INDICATOR = digitalio.DigitalInOut(board.GP24)
+INDICATOR.direction = digitalio.Direction.OUTPUT
 
 
 # State 0 - light is OFF
@@ -32,7 +42,7 @@ while loop:  # Simplified loop condition to while loop
     #CHECK THAT THIS WORKS!! ---------------------------------------------------------------------------
     #update location at 5pm every day
     if location == 0 or time.localtime() == "17:00:00":
-        location == get_location()
+        location = get_location()
     
     # motion is a terminal input for now
     motion = 1 if input("Motion detected? (0/1): ") == "1" else 0
@@ -43,6 +53,7 @@ while loop:  # Simplified loop condition to while loop
     communication = 1 if distance <= range else 0
     #communication = 1 if input("Communication detected? (0/1): ") == "1" else 0
     
+    # similates the end of the night
     if input("End (Y/N): ") == "Y":
         break
     # ---------------------------------------------------------------------------------------------------
@@ -73,6 +84,19 @@ while loop:  # Simplified loop condition to while loop
         if time.time() - wait_start_time >= wait_duration:
             print("state changed to 0")
             state = 0
+    
+
+    #Light control
+    if state == 1 or state == 2:
+        LIGHT.value = True
+    else:
+        LIGHT.value = False
+
+    # If a location is being recieved, light up
+    if motion_location != 0:
+        INDICATOR.value = True
+    else:
+        INDICATOR.value = False
 
     # Delay to avoid hogging the CPU
     time.sleep(1)  # Adjust sleep time as necessary
