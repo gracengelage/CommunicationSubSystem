0db54fc38afe77fc7bd2fd1e4619d581c4361232
Michael Mo <michaelmoshui@gmail.com>
2024-03-20 13:28:55 -0400

    Fix conflict

--- communication_functions/get_location.py
+++ communication_functions/get_location.py
@@ -1,5 +1,3 @@
-import time
-
 # Output Parameters:
 # Location Information ~ a string representing the location of the device in degrees
 #                      ~ "XXXYYYYYYYYDAAABBBBBBBBE"
@@ -11,13 +9,6 @@ import time
 #                      ~ 8 B's are the decimals of the second coordinate
 #                      ~ E denotes hemisphere of second coordinate (N = 0, E = 1, S = 2, W = 3)
 #                      ~ Example: 44.18273845N, 115.12345678W
-<<<<<<< HEAD
-#                      ~ "20441827384503115123456784"
-def get_location() -> str:
-    # GPS needs warm up time, GPS until a valid location is received
-    location_received = False
-=======
 #                      ~ "044182738450115123456783"
 def get_location(last_update=None):
     pass
->>>>>>> 3c3fb9015a1ca8f83bd791301a132e055ad6e0b4
