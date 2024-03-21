7f9f21c7c32afee1b10880e546c664f265ceb67e
Grace Engelage <grace.engelage@gmail.com>
2024-03-21 13:00:45 -0400

    Inputs and outputs defined for next_state

--- communication_functions/update_state.py
+++ communication_functions/update_state.py
@@ -6,6 +6,17 @@ from communication_functions.get_location import get_location
 from communication_functions.signal_transmission import receive_signal
 from communication_functions.calculate_distance import calculate_distance
 
+#######################
+# next_state function
+####################### 
+# outputs are:  current state (0,1 or 2)
+#               wait_start_time (from time.time()) which tells the time that state 2 started
+# inputs are:   current state (0,1 or 2)
+#               motion (0, 1) 1 if motion is detected, otherwise 0
+#               communication (0,1) 1 if communication is detected in range, otherwise 0
+#               wait_start_time (from time.time()) which tells the time that state 2 started
+#               wait_duration (in seconds) how long the light should stay on once there is no motion detected in range
+#               
 def next_state(current_state, motion, communication, wait_start_time, wait_duration):
     if current_state == 0:
         if communication == 1 or motion == 1:
@@ -56,6 +67,9 @@ range_meters = 20  # TBD!!
 
 while True:
 
+    ######################################
+    # Variables that influence the state
+    ######################################
     # CHECK THAT THIS WORKS!! ---------------------------------------------------------------------------
     # Update location at 5 pm every day
     if location == 0 or time.localtime() == "17:00:00":
@@ -75,15 +89,20 @@ while True:
         break
     # ---------------------------------------------------------------------------------------------------
 
-    # Determine the next state
     state, wait_start_time = next_state(state, motion, communication, wait_start_time, wait_duration)
 
+    ################
+    # Big light
+    ################
     # Light control
     if state == 1 or state == 2:
         LIGHT.value = True
     else:
         LIGHT.value = False
 
+    #################
+    # Indicator LED
+    #################
     # If a location is being received, light up
     if motion_location != 0:
         INDICATOR.value = True
@@ -91,4 +110,4 @@ while True:
         INDICATOR.value = False
 
     # Delay to avoid hogging the CPU
-    time.sleep(1)  # Adjust sleep time as necessary
+    time.sleep(1)
